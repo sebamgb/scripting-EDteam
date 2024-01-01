962c483b2e828a71eb71da5eff1cfafa505d4358
debianAdmin <sebamgb@gmail.com>
2023-12-31 21:38:21 -0300

    hello_world.sh: printing hello world

---
+++ hello_world.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# description: print hello world
+# author: Sebastián Romero
+
+name="Sebastián Romero"
+
+echo "Hello world"
+echo "from" $name
+
+exit 0
+
+## exit 0: well done
+## exit 1: general error, like zero division
+## exit 2: shell error
